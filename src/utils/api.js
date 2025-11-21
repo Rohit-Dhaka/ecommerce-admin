58bb13c5725c58169dde53fea0d48f813c280a87
Rohit dhaka <rohitdhaka@gmail.com>
2025-11-21 21:06:37 +0530

    change baseurl

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000/api/v1/'
+    // baseURL: 'http://localhost:4000/api/v1/'
+    baseURL:  'https://ecommerce-backend-nlk4.onrender.com/api/v1'
 });
 
 export default api;
